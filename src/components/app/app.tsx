8332b996999512ea3bbbf7763b7c9bd78c21853a
Фирсов Роман Геннадьевич <RGFirsov@alfabank.ru>
2021-06-28 15:51:10 +0300

    feat: add vinyl record disk

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { createCn } from 'bem-react-classname';
 
 import VinylRecordDisk from '../vinyl-record-disk';
+import imgsrc from '../../test.jpeg';
 
-import './app.css';
+import './app.scss';
 
 const cn = createCn('app');
 
 const App = () => <div className={cn()}>
-    <VinylRecordDisk />
+    <div className={cn('vinyl-player')}>
+        <VinylRecordDisk />
+    </div>
+    <img src={imgsrc} alt='' style={{position: 'absolute',
+    top: 0,
+    zIndex: 0}}/>
 </div>
 
 export default App;
--- src/components/vinyl-record-disk/vinyl-record-disk.tsx
+++ src/components/vinyl-record-disk/vinyl-record-disk.tsx
@@ -1,14 +1,52 @@
 import React from 'react';
 import { createCn } from 'bem-react-classname';
 
-import './vinyl-record-disk.css';
+import VinylRecordInfo from '../vinyl-record-info';
+
+import './vinyl-record-disk.scss';
 
 const cn = createCn('vinyl-record-disk');
 
-const VinylRecordDisk = () => <div className={cn()}>
-    <div className={cn('outer-border')}>
-
-    </div>
-</div>
+const VinylRecordDisk = () => {
+    
+    const renderTrackStripe = (count: number = 0) => {
+        if (count === 25) {
+            return null;
+        }
+        
+        return (
+            <div className={cn('vinyl-track-stripe')}>
+                {renderTrackStripe(count + 1)}
+            </div>
+        )
+    }
+    
+    return (
+        <div className={cn()}>
+            <div className={cn('tracks')}>
+                {renderTrackStripe()}
+            </div>
+            <div className={cn('vinyl-tracks-shadow')}>
+                <div className={cn('shadow-wrapper')}>
+                    <div className={cn('small-cirlce')}/>
+                    <div className={cn('big-circle')}/>
+                </div>
+                <div className={cn('shadow-wrapper', {inverted: true})}>
+                    <div className={cn('small-cirlce')}/>
+                    <div className={cn('big-circle')}/>
+                </div>
+            </div>
+            <div className={cn('vinyl-tracks-flare')}>
+                <div className={cn('flare-sector')} />
+                <div className={cn('flare-sector', {inverted: true})} />
+            </div>
+            <div className={cn('outer-track-border')}>
+                <div className={cn('inner-track-border')}>
+                    <VinylRecordInfo />
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default VinylRecordDisk;
